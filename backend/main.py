b7abc24d4165715c04e99cb81a1e5582459b1d3a
Akkilesh-A <akkilalagar05@gmail.com>
2024-07-04 23:43:11 +0530

    project setup

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from flask import Flask,request, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Your route definitions
+@app.route('/', methods=['GET'])
+def healthy_server():
+    return jsonify({"message": "Healthy server running"})
+
+if __name__ == '__main__':
+    app.run(debug=True)

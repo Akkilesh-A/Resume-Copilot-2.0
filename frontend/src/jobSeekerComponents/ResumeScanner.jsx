49978b9a830b92d14a034252fd96cde2e984ea46
Akkilesh-A <akkilalagar05@gmail.com>
2024-07-07 17:13:44 +0530

    frontend routes added

--- frontend/src/jobSeekerComponents/ResumeScanner.jsx
+++ frontend/src/jobSeekerComponents/ResumeScanner.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BlackButton, WhiteButton } from '../components';
 
 function ResumeScanner() {
     const [image, setImage] = useState('');
@@ -13,6 +15,7 @@ function ResumeScanner() {
         result: ''
       } )
     const [loading,setLoading] =useState(false)
+    const navigate =useNavigate()
 
     function handleImage(e) {
         setImage(e.target.files[0]);
@@ -25,7 +28,7 @@ function ResumeScanner() {
         formData.append('jobTitle', jobTitle);
         formData.append('techStack', techStack);
 
-        const url = "https://resume-copilot-2-0.onrender.com/resume_scan_with_ai"; // Ensure this matches your Flask route
+        const url = "http://localhost:5000/resume_scan_with_ai"; // Ensure this matches your Flask route
         const options = {
             method: 'POST',
             body: formData
@@ -34,12 +37,13 @@ function ResumeScanner() {
         try {
             const response = await fetch(url, options);
             const data = await response.json();
-
             if (response.ok) {
                 setData({message:data.message,jobTitle:jobTitle,techStack:techStack,score:data.score,result:data.result})  
                 await localStorage.setItem('resumeScoreData',JSON.stringify(finalData))    
-                setLoading(false)          
-                window.location.href=`/resumescore`
+                setTimeout(()=>{
+                    setLoading(false)          
+                    navigate(`/resumescore`)
+                },3000)
             } else {
                 setLoading(false)
             }
@@ -49,36 +53,46 @@ function ResumeScanner() {
     }
 
     return (
-        <div className='mt-24 mx-8'>
-            <div className='flex justify-center items-center mb-12'>
-                <h1 className='text-[2.5em] font-extrabold'>🧑🏻‍🏫 Get your Resume Score here! 👩🏻‍🏫</h1>
+        <div className="flex justify-center h-screen w-screen items-center flex-col bg-black ">
+            <div className='bg-white rounded p-8 '>
+                <div className='flex justify-center items-center mb-12'>
+                    <h1 className='text-[2.5em] font-extrabold'>Get your Resume Score here!</h1>
+                </div>
+                <div className='flex flex-col justify-around items-center mb-12'> 
+                    <table className='text-center'>
+                        <thead>
+                            <tr>
+                                <th className='p-4'>Job Position</th>
+                                <td><input className='border-2 border-black p-2 rounded w-[20vw]' type="text" name="jobTitle" id='jobTitle' value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} /></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th className='p-4'>Tech Stacks</th>
+                                <td><textarea rows="5" className='border-2 border-black p-2 rounded w-[20vw]' type="text" name="techStack" id='techStack' value={techStack} onChange={(e) => setTechStack(e.target.value)} placeholder='Enter Tech Stacks each seperated by a comma' /></td>
+                            </tr>
+                            <tr>
+                                <th className='p-4'>Resume</th>
+                                <td><input type='file' name='file'className='border-2 border-black p-2 rounded w-[20vw]' onChange={handleImage} /></td>
+                            </tr>
+                        </tbody>
+                    </table> 
+                    <div className='flex gap-4 justify-center items-center' colSpan={2}>
+                        <button onClick={handleApi} className='mt-4 hover:bg-black hover:fill-white gap-4 hover:text-white flex justify-center items-center rounded-xl px-4 p-2 border-4 border-black font-bold' >
+                            Upload & Process
+                        </button>
+                        {loading && <div className='animate-spin p-4 rounded-full border-black border-4 border-dotted'>🤖</div>}
+                    </div>       
+                </div>   
             </div>
-            <div className='flex flex-col justify-around items-center mb-12'> 
-                <table className='text-center'>
-                    <thead>
-                        <tr>
-                            <th className='p-4'>Job Position</th>
-                            <td><input className='border-2 border-black p-2 rounded w-[20vw]' type="text" name="jobTitle" id='jobTitle' value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} /></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th className='p-4'>Tech Stacks</th>
-                            <td><textarea rows="5" className='border-2 border-black p-2 rounded w-[20vw]' type="text" name="techStack" id='techStack' value={techStack} onChange={(e) => setTechStack(e.target.value)} placeholder='Enter Tech Stacks each seperated by a comma' /></td>
-                        </tr>
-                        <tr>
-                            <th className='p-4'>Resume</th>
-                            <td><input type='file' name='file'className='border-2 border-black p-2 rounded w-[20vw]' onChange={handleImage} /></td>
-                        </tr>
-                    </tbody>
-                </table> 
-                <div className='flex flex-col justify-center items-center' colSpan={2}>
-                    <button onClick={handleApi} className='mt-4 hover:bg-black hover:fill-white  hover:text-white flex justify-center items-center rounded-xl px-4 p-2 border-4 border-black font-bold' >
-                        Upload ⬆️ & Process 🤖
-                    </button>
-                    {loading && <div className='mt-8 animate-spin p-4 rounded-full border-black border-4 border-dotted'></div>}
-                </div>       
-            </div>   
+            <div className='md:visible invisible'>
+                <div className='absolute bottom-10 right-10'>
+                    <BlackButton buttonText={"Go Back"} link={"menupage"} />
+                </div>   
+                <div className='absolute bottom-10 right-48'>
+                    <BlackButton buttonText={"Home"} link={""} />
+                </div> 
+            </div>
         </div>        
     );
 }
--- frontend/src/jobSeekerComponents/ResumeScore.jsx
+++ frontend/src/jobSeekerComponents/ResumeScore.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { BlackButton } from '../components';
 
 const ResumeScore = () => {
     const [data, setData] = useState(null);
@@ -13,21 +14,41 @@ const ResumeScore = () => {
         getData()
     }, []);
 
-    if (!data) {
-        return <p>Loading...</p>;
-    }
-
     return (
-        <div className='bg-black text-white flex justify-center items-center flex-col w-screen h-screen gap-4'>
-            <h1 className='text-6xl font-bold'>Get your resume score here</h1>
-            <div>
-                <p className='text-[1.2rem]'><strong>Job Title:</strong> {data.jobTitle}</p>
-                <p className='text-[1.2rem]'><strong>Tech Stack:</strong> {data.techStack}</p>
-                <p className='text-[1.2rem]'><strong>Missing Keywords:</strong> {data.missingKeywords}</p>
-                <p className='text-[1.2rem]'><strong>Score:</strong> {data.score}</p>
-                <p className='text-[1.2rem]'><strong>Result:</strong> {data.result}</p>
-            </div>
+        // <div className='bg-black text-white flex justify-center items-center flex-col w-screen h-screen gap-4'>
+        //     <h1 className='text-6xl font-bold'>Get your resume score here</h1>
+        //     {data && <div>
+        //         <p className='text-[1.2rem]'><strong>Job Title:</strong> {data.jobTitle}</p>
+        //         <p className='text-[1.2rem]'><strong>Tech Stack:</strong> {data.techStack}</p>
+        //         <p className='text-[1.2rem]'><strong>Missing Keywords:</strong> {data.missingKeywords}</p>
+        //         <p className='text-[1.2rem]'><strong>Score:</strong> {data.score}</p>
+        //         <p className='text-[1.2rem]'><strong>Result:</strong> {data.result}</p>
+        //     </div>}
+
+        // </div>
+        <div className='bg-black md:h-screen text-white  w-screen pt-16'>
+        <div className=' text-center text-[2.5em] font-bold '>
+            Your Resume Score
         </div>
+        <div className='flex flex-wrap flex-col justify-center p-32 '>
+                {data && 
+                <div className="flex flex-col gap-8">
+                    <p className='text-[1.7rem]'><strong>Job Title:</strong> {data.jobTitle}</p>
+                    <p className='text-[1.7rem]'><strong>Tech Stack:</strong> {data.techStack}</p>
+                    <p className='text-[1.7rem]'><strong>Missing Keywords:</strong> {data.missingKeywords}</p>
+                    <p className='text-[1.7rem]'><strong>Score:</strong> {data.score}</p>
+                    <p className='text-[1.7rem]'><strong>Result:</strong> {data.result}</p>
+                </div>}
+        </div>
+        <div className='md:visible invisible absolute bottom-10 right-10 flex gap-4'>
+            <div className=''>
+                <BlackButton buttonText={"Try with another?"} link={"resumescoreform"} />
+            </div>   
+            <div className=''>
+                <BlackButton buttonText={"Home"} link={""} />
+            </div> 
+        </div> 
+    </div>
     );
 };
 

2d45d5b9e269d825cc84972cccf0820a3591d3b1
Akkilesh-A <akkilalagar05@gmail.com>
2024-07-07 14:31:49 +0530

    Updated backend links

--- frontend/src/jobSeekerComponents/ResumeScore.jsx
+++ frontend/src/jobSeekerComponents/ResumeScore.jsx
@@ -15,15 +15,14 @@ const ResumeScore = () => {
     }
 
     return (
-        <div>
-            <h1>Get your resume score here</h1>
+        <div className='bg-black text-white flex justify-center items-center flex-col w-screen h-screen gap-4'>
+            <h1 className='text-6xl font-bold'>Get your resume score here</h1>
             <div>
-                <p><strong>Message:</strong> {data.message}</p>
-                <p><strong>Job Title:</strong> {data.jobTitle}</p>
-                <p><strong>Tech Stack:</strong> {data.techStack}</p>
-                <p><strong>Missing Keywords:</strong> {data.missingKeywords}</p>
-                <p><strong>Score:</strong> {data.score}</p>
-                <p><strong>Result:</strong> {data.result}</p>
+                <p className='text-[1.2rem]'><strong>Job Title:</strong> {data.jobTitle}</p>
+                <p className='text-[1.2rem]'><strong>Tech Stack:</strong> {data.techStack}</p>
+                <p className='text-[1.2rem]'><strong>Missing Keywords:</strong> {data.missingKeywords}</p>
+                <p className='text-[1.2rem]'><strong>Score:</strong> {data.score}</p>
+                <p className='text-[1.2rem]'><strong>Result:</strong> {data.result}</p>
             </div>
         </div>
     );
